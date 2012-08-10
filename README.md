55dfa93c39aab3114e0a8553c2e6b35ee758d171
Mario Steinhoff <mario.steinhoff@catapullt.de>
2012-08-10 20:34:25 +0200

    Add support for multiple crop boxes
    Add support for multiple crop boxes on the same image
    Move static style into own file
    Update README.md with new usage
    Remove minified version
    
    Signed-off-by: Mario Steinhoff <mario.steinhoff@catapullt.de>

--- README.md
+++ README.md
@@ -1,22 +1,107 @@
 # jQuery UI Croppable - A plugin for web-based image cropping
 
+## WARNING: alpha version
+This version is not intended for production use.
+If you want to use it anyway, make sure you understand the source code completely.
+
 ## What exactly does it do?
-When called on an jquery object of an image, Croppable creates the necessary elements and javascript to make a nice interface for determining the dimensions of a cropped image.
+When called on an jQuery object of an image, Croppable creates the necessary elements and JavaScript to make a nice interface for determining the dimensions of a cropped image. I needed support for multiple crop handles within a single image and neither jCrop nor the original Croppable seemed to support this, so I wrote it myself.
 
 ## What do I need to use it?
-You'll need jQuery and jQueryUI (I developed Croppable with versions 1.7.2 and 1.8.7 respectively). You'll also need a backend that can handle image cropping.
+You'll need jQuery, jQuery UI and some backend that supports cropping. Croppable returns an coordinate object with left/top/width/height values of the crop box. I developed this with jQuery 1.7.1 and jQuery UI 1.8.22.
 
 ## How do I use it?
-Call the plugin on an image and pass it an element to click to initiate the crop and a callback to handle the output of the plugin. The dimString sent to the callback is in the form of (cropped width)x(cropped height)+(left offset)+(top offset). This happens to be the precise format to pass to ImageMagick when using a -crop command.
+Call the plugin on an image and pass it some configuration options.
 
 ###Example:
 ```javascript
-function cropCallback(dimString) {
-    // do some things with the data
-}
+$('#some-image').croppable(
+{
+  handles: {
+    handle1: {
+      // background color of the selection
+      bgColor: 'red',
+      
+       // used for Resizable minWidth and minHeight. Currently required.
+      minSize : {width:200,height:50},
+      
+      // used for Resizable aspectRatio
+      aspectRatio: true,
+      
+       // when given, displays a little preview box in the given element
+       // the bgColor is used as the border color for the preview
+      $preview: $( '#handle1-preview' ), 
+      
+      // set the initial position, width and height of the frame
+      initialSelection: {
+        left: 0,
+        top: 0,
+        width: frames.handset.width,
+        height: frames.handset.height
+      }
+    },
+    handle2: {
+      ...
+    }
+  }
+});
+```
 
-$('#some-image').croppable({ 'cropSelector': '#crop-button' }, cropCallback);
+You can listen for coordinates using standard jQuery events.
+
+# Events to bind on
+The parameters are the same on both events:
+
+* evt is the original event
+* box is an object representing the box
+* coords is an representing the box coordinates at the time the event fired
+
+The events derive from jQuery UI resizeable (start, resize, stop) and draggable (start, drag, stop).
+
+## croppable.change
+This event fires everytime the crop box size is changed.
+
+When preview is activated for the crop box, this event is used internally for updating the preview.
+
+_Example_
+```javascript
+$('#some-image').on(
+  'croppable.change,
+  function( evt, box, coords )
+  {
+    console.log( box.name, 'changed its coordinates: ', coords.left, coords.top, coords.width, coords.height );
+  }
+);
+```
+
+## croppable.select
+This event fires when the user has finished a crop box resize.
+
+_Example_
+In this example, the event is used to populate the crop values to hidden input fields like in jcrop.
+
+```javascript
+$('#some-image').on(
+  'croppable.select',
+  function( evt, box, coords )
+  {
+    $( '#' + box.name + '-crop-left' ).val( coords.left );
+    $( '#' + box.name + '-crop-top' ).val( coords.top );
+    $( '#' + box.name + '-crop-width' ).val( coords.width );
+    $( '#' + box.name + '-crop-height' ).val( coords.height );
+  }
+);
 ```
 
 ## What kind of backend can I use to crop the image?
-I developed Croppable using a [Sinatra](http://www.sinatrarb.com/) instance and [MiniMagick](https://github.com/probablycorey/mini_magick). You can use whatever backend you want. You don't even really need to use a backend at all - dump the output to the console if you really want.
+See [original repository](https://github.com/EvilScott/jquery-ui-croppable).
+
+## Known issues
+# Resize to top/left
+The jQuery UI resizeable plugin seems to be buggy when resizing is used with an combination of containment and minWidth/minHeight.
+When a crop box is at the top/left corner and the users pulls the resize handle to top/left, the calculation messes up and the box gets smaller than minWidth/minHeight. When changing the size again, the box jumps back to minWidth/minHeight.
+
+# Minified version missing
+I was to lazy to include one. Also, minified versions can be generated easily, and I think resources that can be generated should not be put under version control.
+
+Currently only tested in Chrome 21.0.x.
--- jquery-ui-croppable-1.1.0.js
+++ jquery-ui-croppable-1.1.0.js
@@ -1,145 +1,417 @@
-// create jQuery function using Croppable
-(function ( $ ) {
+/**
+ * jQuery UI croppable plugin
+ * 
+ * https://github.com/EvilScott/jquery-ui-croppable
+ * 
+ * Customized version with multicrop support
+ */
+(function( $ )
+{
+  var self, Croppable;
+  
+  self = {};
+  Croppable = {
+    /**
+     * Default options
+     * 
+     * TODO
+     */
+    defaults: {
+    },
+    
+    /**
+     * Initialize the croppable plugin.
+     * 
+     * - Create container element
+     * - Add CSS classes with static css properties
+     * - Create cropping handles
+     * 
+     * @param image The image that should be cropped
+     * @param options The configuration object
+     */
+    init: function( image, options )
+    {
+      var boxIndex;
+    
+      self.options = options;
+    
+      // ------------------------------------------------------------------
+      // prepare image
+      // ------------------------------------------------------------------
+      self.$image = image;
+      self.$image.addClass( 'croppable-image' );
+      
+      self.containerSize = {
+        width: self.$image.width(),
+        height: self.$image.height()
+      };
 
-    var Croppable = {
-
-        // size holding div, line up resizer div, and enable button
-        init: function(image, options) {
-
-            // save image for crop to use
-            self.image = image;
-
-            // create cropping box and resize handle
-            self.image.parent().append('<div id="crop-box">' +
-                '<div class="ui-resizable-handle ui-resizable-se" id="resize-handle-se"></div>' +
-                '<div class="ui-resizable-handle ui-resizable-sw" id="resize-handle-sw"></div>' +
-                '<div class="ui-resizable-handle ui-resizable-nw" id="resize-handle-nw"></div>' +
-                '<div class="ui-resizable-handle ui-resizable-ne" id="resize-handle-ne"></div>' +
-            '</div>');
-
-            // style crop box, resize handle, and image
-            $('#crop-box').css({
-                'z-index':    '1000',
-                'position':   'absolute',
-                'cursor':     'move',
-                'background-image': 'url("' + self.image.attr('src') + '")',
-                'background-repeat': 'no-repeat',
-                'background-attachment': 'fixed'
-            });
-            $('#crop-box #resize-handle-se').css({
-                'z-index':    '1001',
-                'position':   'absolute',
-                'bottom':     '0',
-                'right':      '0',
-                'cursor':     'se-resize',
-                'height':     '25px',
-                'width':      '25px',
-                'background': '#B94A48'
-            });
-            $('#crop-box #resize-handle-sw').css({
-                'z-index':    '1001',
-                'position':   'absolute',
-                'bottom':     '0',
-                'left':       '0',
-                'cursor':     'sw-resize',
-                'height':     '25px',
-                'width':      '25px',
-                'background': '#B94A48'
-            });
-            $('#crop-box #resize-handle-nw').css({
-                'z-index':    '1001',
-                'position':   'absolute',
-                'top':        '0',
-                'left':       '0',
-                'cursor':     'nw-resize',
-                'height':     '25px',
-                'width':      '25px',
-                'background': '#B94A48'
-            });
-            $('#crop-box #resize-handle-ne').css({
-                'z-index':    '1001',
-                'position':   'absolute',
-                'top':        '0',
-                'right':      '0',
-                'cursor':     'ne-resize',
-                'height':     '25px',
-                'width':      '25px',
-                'background': '#B94A48'
-            });
-            self.image.css({
-                'opacity': '0.40',
-                'filter': 'alpha(opacity=40)'
-            });
-            self.image.parent().css({
-                'position': 'relative'
-            });
-
-            // line up background with possible scrolled page
-            Croppable.moveBackground();
-
-            // crop box initial position and size
-            $('#crop-box').offset({
-                top:  self.image.offset().top + (self.image.innerHeight() * 0.05),
-                left: self.image.offset().left + (self.image.innerWidth() * 0.05)
-            });
-            $('#crop-box').css({
-                'height': (self.image.innerHeight() * 0.90) + 'px',
-                'width':  (self.image.innerWidth() * 0.90) + 'px'
-            });
-
-            var resizableOptions = {
-                containment: self.image,
-                handles: options.resizeHandles,
-                resize: options.resizeCallback
-            };
-
-            // set crop box to be draggable and resizeable
-            $('#crop-box').draggable({ containment: self.image })
-                          .resizable(resizableOptions);
-        },
-
-        // send new image dimensions via post
-        crop: function() {
-            var height = $('#crop-box').innerHeight();
-            var width = $('#crop-box').innerWidth();
-            var left = $('#crop-box').position().left - self.image.position().left;
-            var top = $('#crop-box').position().top - self.image.position().top;
-            var dimString = width + "x" + height + "+" + left + "+" + top;
-            Croppable.cropCallback.call(this, dimString);
-        },
-
-        // move background when window scrolls
-        moveBackground: function() {
-            var top = self.image.offset().top - $(document).scrollTop();
-            $('#crop-box').css({
-                'background-position': self.image.offset().left + 'px ' + top + 'px'
-            });
-        },
-
-        // default options
-        defaults: {
-            resizeCallback: function() {},
-            resizeHandles: {
-                'se': '#resize-handle-se',
-                'sw': '#resize-handle-sw',
-                'nw': '#resize-handle-nw',
-                'ne': '#resize-handle-ne'
-            }
+      self.$container = $('<div/>').addClass( 'croppable-container' );
+      self.$container.width( self.containerSize.width );
+      self.$container.height( self.containerSize.height );
+      
+      self.$image.parent().append( self.$container );
+      self.$container.append( self.$image );
+      
+      // ------------------------------------------------------------------
+      // Create crop boxes
+      // ------------------------------------------------------------------
+      boxIndex = 0;
+      
+      self.boxState = [];
+      $.each(
+        options.handles,
+        function( name, data )
+        {
+          self.boxState[ boxIndex++ ] = Croppable.createCropBox( name, data );
         }
-    };
+      );
+        
+      // activate first cropbox
+      Croppable.setBoxFocus( self.boxState[0] );
+        
+      // ------------------------------------------------------------------
+      // Wire preview update event
+      // ------------------------------------------------------------------
+      self.$image.on(
+        'croppable.change',
+        function( evt, box, coords )
+        {
+          // x/y shrink factor 
+          var sfx, sfy;
+          
+          sfx = box.previewSize.width / coords.width;
+          sfy = box.previewSize.height / coords.height;
+          
+          box.$previewImage.css({
+            width: sfx * self.containerSize.width + 'px',
+            marginLeft: '-' + sfx * coords.left + 'px',
+            height: sfy * self.containerSize.height  + 'px',
+            marginTop: '-' + sfy * coords.top + 'px'
+          });
+        }
+      );
+      
+      // ------------------------------------------------------------------
+      // Wire focus management
+      // ------------------------------------------------------------------
+      self.$container.on(
+        'click',
+        function( evt )
+        {
+          var xco, yco, $cropbox, pos, containing, cbt, cbl, cbw, cbh, needFocus;
+          
+          // 1. get current click position
+          xco = evt.pageX;
+          yco = evt.pageY;
+          
+          console.log( 'clicked at', xco, yco );
+          
+          // 2. get boxes which should be included in cycling
+          containing = [];
+          for( var i = 0, len = self.boxState.length; i < len; i++ )
+          {
+            $cropbox = self.boxState[i].$cropbox;
+            
+            pos = $cropbox.offset();
+            cbl = pos.left;
+            cbt = pos.top;
+            cbw = $cropbox.width();
+            cbh = $cropbox.height();
+            
+            console.log( 'cropbox', i, ' rectancle:', cbl, cbt, cbw, cbh );
+             
+            // clicked within box, consider for cycling 
+            if( xco >= cbl && xco <= (cbl+cbw)
+             && yco >= cbt && yco <= cbt+cbh )
+            {
+              console.log( 'cropbox match', i, ' rectancle:', pos.left, pos.top, $cropbox.width(), $cropbox.height() );
+              containing.push( self.boxState[i] );
+            }
+          }
+          
+          // apply focus to the eligible box
+          needFocus = true;
+          for( var i = 0, len = containing.length; i < len; i++ )
+          {
+            if ( containing[i].active )
+            {
+              needFocus = false;
+              
+              if( typeof containing[i+1] !== 'undefined' )
+              {
+                Croppable.setBoxFocus( containing[i+1] );
+              }
+              else
+              {
+                Croppable.setBoxFocus( containing[0] );
+              }
+              
+              break;
+            }
+          }
+          
+          if( needFocus )
+          {
+            Croppable.setBoxFocus( containing[0] );
+          }
+        }
+      );
+    },
+    
+    /**
+     * Puts the focus on the given box
+     * 
+     * @param box A box created by Croppable.createCropBox()
+     */
+    setBoxFocus: function( box )
+    {
+      $.each(
+        self.boxState,
+        function( index, box )
+        {
+          box.active = false;
+          box.$cropbox.removeClass( 'active' );
+        }
+      );
+      
+      box.active = true;
+      box.$cropbox.addClass( 'active' );
+    },
+    
+    /**
+     * Create a cropping box
+     * 
+     * @param name The internal name of the box
+     * @param options The box configuration
+     * 
+     * @return An object with the current box state
+     */
+    createCropBox: function( name, options )
+    {
+      var box, $cropbox, size, offset;
+      
+      box = {};
+      
+      box.name = name;
+      box.options = options;
+      
+      // ------------------------------------------------------------------
+      // create cropping box HTML structure
+      // - cropping box
+      // - color gobo
+      // - cropping handles
+      // ------------------------------------------------------------------
+      box.$cropbox = $(
+        '<div id="croppable-box-' + name + '" class="croppable-box">' +
+          '<div class="croppable-box-gobo"></div>' +
+          '<div class="ui-resizable-handle ui-resizable-ne"></div>' +
+          '<div class="ui-resizable-handle ui-resizable-n"></div>' +
+          '<div class="ui-resizable-handle ui-resizable-nw"></div>' +
+          '<div class="ui-resizable-handle ui-resizable-w"></div>' +
+          '<div class="ui-resizable-handle ui-resizable-sw"></div>' +
+          '<div class="ui-resizable-handle ui-resizable-s"></div>' +
+          '<div class="ui-resizable-handle ui-resizable-se"></div>' +
+          '<div class="ui-resizable-handle ui-resizable-e"></div>' +
+          '<div class="croppable-line croppable-hline"></div>' +
+          '<div class="croppable-line croppable-hline bottom"></div>' +
+          '<div class="croppable-line croppable-vline"></div>' +
+          '<div class="croppable-line croppable-vline right"></div>' +
+        '</div>'
+      );
+      
+      // ------------------------------------------------------------------
+      // Apply dynamic CSS properties
+      // ------------------------------------------------------------------
+      //box.$cropbox.css( {backgroundImage: 'url("' + self.$image.attr('src') + '")'} );
+      $( '.croppable-box-gobo', box.$cropbox ).css( {backgroundColor: options.bgColor} );
+      
+      // line up background with possible scrolled page
+      Croppable.moveBackground( box );
+      
+      // ------------------------------------------------------------------
+      // Set initial frame position
+      // ------------------------------------------------------------------
+      if( options.initialSelection )
+      {
+        size = {
+          width: options.initialSelection.width,
+          height: options.initialSelection.height
+        };
+        
+        offset = {
+          left: options.initialSelection.left,
+          top: options.initialSelection.top
+        };
+      }
+      else
+      {
+        size = {
+          width: options.minSize.width + 'px',
+          height: options.minSize.height + 'px'
+        };
+        
+        offset = self.$image.offset();
+      }
+      
+      box.$cropbox.css( size );
+      box.$cropbox.offset( offset );
+      
+      // ------------------------------------------------------------------
+      // attach to workspace
+      // ------------------------------------------------------------------
+      self.$image.parent().append( box.$cropbox );
+      box.$cropbox.data( 'croppable-box', box );
+      
+      // ------------------------------------------------------------------
+      // Initialize resizable
+      // ------------------------------------------------------------------
+      box.$cropbox.resizable({
+        containment: self.$image,
+        aspectRatio: options.aspectRatio,
+        minWidth: options.minSize.width,
+        minHeight: options.minSize.height,
+        handles: {
+          'ne': $( '.ui-resizable-handle.ui-resizable-ne', box.$cropbox ),
+          'n': $( '.ui-resizable-handle.ui-resizable-n', box.$cropbox ),
+          'nw': $( '.ui-resizable-handle.ui-resizable-nw', box.$cropbox ),
+          'w': $( '.ui-resizable-handle.ui-resizable-w', box.$cropbox ),
+          'sw': $( '.ui-resizable-handle.ui-resizable-sw', box.$cropbox ),
+          's': $( '.ui-resizable-handle.ui-resizable-s', box.$cropbox ),
+          'se': $( '.ui-resizable-handle.ui-resizable-se', box.$cropbox ),
+          'e': $( '.ui-resizable-handle.ui-resizable-e', box.$cropbox ),
+        },
+        
+        start: function( evt, ui )
+        {
+          self.$image.trigger( 'croppable.change', [box, Croppable.makeCoords( ui.helper, ui.position )] );
+        },
+        resize: function( evt, ui )
+        {
+          self.$image.trigger( 'croppable.change', [box, Croppable.makeCoords( ui.helper, ui.position )] );
+        },
+        stop: function( evt, ui )
+        {
+          self.$image.trigger( 'croppable.select', [box, Croppable.makeCoords( ui.helper, ui.position )] );
+        }
+      });
+      
+      // ------------------------------------------------------------------
+      // Initialize draggable
+      // ------------------------------------------------------------------
+      box.$cropbox.draggable({
+        containment: self.$image,
+        
+        start: function( evt, ui )
+        {
+          self.$image.trigger( 'croppable.change', [box, Croppable.makeCoords( ui.helper, ui.position )] );
+        },
+        drag: function( evt, ui )
+        {
+          self.$image.trigger( 'croppable.change', [box, Croppable.makeCoords( ui.helper, ui.position )] );
+        },
+        stop: function( evt, ui )
+        {
+          self.$image.trigger( 'croppable.select', [box, Croppable.makeCoords( ui.helper, ui.position )] );
+        }
+      });
+      
+      // ------------------------------------------------------------------
+      // Initialize preview if requested
+      // ------------------------------------------------------------------
+      if( options.$preview )
+      {
+        if( !options.previewSize )
+        {
+          options.previewSize = options.minSize;
+        }
+        
+        box.$previewImage = self.$image.clone().removeClass();
+        box.$preview = options.$preview;
+        box.previewSize = options.previewSize;
+        box.$preview.append(
+          $( '<div class="croppable-preview"></div>' )
+          .css(
+            {
+              width: options.previewSize.width,
+              height: options.previewSize.height,
+              borderColor: options.bgColor
+            }
+          )
+          .append( box.$previewImage )
+        );
+      }
+      
+      // ------------------------------------------------------------------
+      // Setup focus handling
+      // ------------------------------------------------------------------
+      box.$cropbox.on(
+        'mousedown',
+        function( evt )
+        {
+          /*if( $(evt.target).parent().hasClass('active') )
+          {
+            console.log( evt );
+            console.log( $cropbox.offset().left, $cropbox.offset().top, $cropbox.width(), $cropbox.height() );
+            console.log($('.croppable-box:not(.active)', self.$container));
+          }*/
+          
+          Croppable.setBoxFocus( box );
+        }
+      );
+      
+      box.$preview.on(
+        'click',
+        function( evt )
+        {
+          Croppable.setBoxFocus( box );
+        }
+      );
+      
+      return box;
+    },
+    
+    /**
+     * Creates a coords object from the given data
+     * 
+     * @param $element The container element for width and height
+     * @param position The position object for top and left
+     */
+    makeCoords: function( $element, position )
+    {
+      return {
+        top: position.top,
+        left: position.left,
+        width: $element.width(),
+        height: $element.height()
+      }
+    }
+  };
 
-    // user end of croppable
-    $.fn.croppable = function(options, cropCallback) {
-        var options = $.extend(true, {}, Croppable.defaults, options);
+  /*
+   * user end of croppable
+   * 
+   * @param options The configuration object
+   */
+  $.fn.croppable = function( options )
+  {
+    var options = $.extend( true, {}, Croppable.defaults, options );
 
-        Croppable.init(this, options);
-        Croppable.cropCallback = cropCallback;
+    Croppable.init(this, options);
 
-        $(options.cropSelector).click(Croppable.crop);
-        $(window).scroll(function(){
-            Croppable.moveBackground();
-        });
-        return this;
-    };
-
+    $(window).scroll(
+      function()
+      {
+        $.each(
+          self.boxState,
+          function( index, box )
+          {
+            Croppable.moveBackground( box );
+          }
+        );
+      }
+    );
+    
+    return this;
+  };
 })( jQuery );
